1cf0c164435afbb101a2406c1657ec691acdad75
Billhernandez <pagos2208@gmail.com>
2023-05-30 21:09:22 -0600

    actualizacion
    actualizacion

--- app/src/main/java/sv/edu/utec/proyectops/MainActivity.java
+++ app/src/main/java/sv/edu/utec/proyectops/MainActivity.java
@@ -1,20 +1,34 @@
 package sv.edu.utec.proyectops;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button entrar,registrar;
-
+    EditText correo,contraseña;
+    FirebaseAuth mAut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAut = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.edtNombre);
+        contraseña= findViewById(R.id.edtContra);
 
         entrar = findViewById(R.id.btnAddU);
         registrar = findViewById(R.id.btnRegistro);
@@ -24,8 +38,19 @@ public class MainActivity extends AppCompatActivity {
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intento=new Intent(getApplicationContext(),Home.class);
-                startActivity(intento);
+
+                String User = correo.getText().toString();
+                String Pass = contraseña.getText().toString();
+
+                if(User.isEmpty() && Pass.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Ingrese los datos", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    loginUser(User,Pass);
+
+                }
+
             }
         });
 
@@ -37,4 +62,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void loginUser(String User, String Pass) {
+        mAut.signInWithEmailAndPassword(User,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    finish();
+
+                    Intent intento=new Intent(getApplicationContext(),Home.class);
+
+                    startActivity(intento);
+                    Toast.makeText(MainActivity.this, "BienVenido", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
