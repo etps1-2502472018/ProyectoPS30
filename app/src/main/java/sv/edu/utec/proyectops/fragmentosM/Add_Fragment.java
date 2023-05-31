9130258ec6b4dbc4b19f8ea6e6c3053e468a46b6
Billhernandez <pagos2208@gmail.com>
2023-05-30 21:56:15 -0600

    actualizacion2

--- app/src/main/java/sv/edu/utec/proyectops/fragmentosM/Add_Fragment.java
+++ app/src/main/java/sv/edu/utec/proyectops/fragmentosM/Add_Fragment.java
@@ -7,19 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import sv.edu.utec.proyectops.R;
 
 
 public class Add_Fragment extends Fragment {
 
+    EditText edtnombre,edtcorreo,edtcontraseña;
+    Button agregar;
 
+    FirebaseDatabase mDatabase;
+    FirebaseAuth mAuth;
+
+    private String nombre="",correo="",contraseña="";
     public Add_Fragment() {
         // Required empty public constructor
+
     }
-
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,10 @@ public class Add_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_, container, false);
+        edtnombre = (EditText) view.findViewById(R.id.edtnombre1);
+        edtcorreo = (EditText) view.findViewById(R.id.edtcorreo1);
+        edtcontraseña = (EditText) view.findViewById(R.id.edtpass1);
+        return view;
     }
 }
